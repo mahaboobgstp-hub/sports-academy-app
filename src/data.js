dcfea5005ab15266741d7ddf6b70d5053ab57e50
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-20 23:57:04 +0530

    Create data.js
    add static data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const SPORTS = ["Basketball", "Badminton"];
+
+export const SEASONS = [
+  "Season 1",
+  "Season 2",
+  "Season 3",
+  "Summer Camp"
+];
+
+export const EMIRATES = [
+  "Abu Dhabi",
+  "Dubai",
+  "Sharjah",
+  "Ajman",
+  "Fujairah",
+  "Ras Al Khaimah",
+  "Umm Al Quwain"
+];
+
+export const LOCATIONS = Array.from({ length: 30 }, (_, i) => ({
+  id: i + 1,
+  name: `School Location ${i + 1}`
+}));
