1a76918966d53b2b19c2226155f6299f21dcd85b
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-23 21:42:36 +0530

    Update Placeholder.jsx

--- src/pages/Placeholder.jsx
+++ src/pages/Placeholder.jsx
@@ -1,1 +1,13 @@
+import AppLayout from "../layout/AppLayout";
+
+export default function Placeholder({ title }) {
+  return (
+    <AppLayout>
+      <h2>{title}</h2>
+      <div className="placeholder">
+        UI will be implemented in next phase.
+      </div>
+    </AppLayout>
+  );
+}
 
