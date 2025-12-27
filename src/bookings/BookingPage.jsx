c87503628aa329fac9e99d48eb23ffd32e5f011b
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-27 23:03:50 +0530

    Create BookingPage.jsx

---
+++ src/bookings/BookingPage.jsx
@@ -0,0 +1,11 @@
+import "./BookingPage.css";
+
+export default function BookingPage() {
+  return (
+    <div className="booking-root">
+      <h1>Booking Page</h1>
+      <p>If you can see styling, CSS is working.</p>
+      <button className="test-btn">Test Button</button>
+    </div>
+  );
+}
