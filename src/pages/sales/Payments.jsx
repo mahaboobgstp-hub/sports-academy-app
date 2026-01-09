1c39b49559505e61c16c742822a9a591c6df911b
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-10 02:29:10 +0530

    Update Payments.jsx

--- src/pages/sales/Payments.jsx
+++ src/pages/sales/Payments.jsx
@@ -1,1 +1,17 @@
+import PaymentSummaryCards from "../../components/sales/PaymentSummaryCards";
+import PaymentFilters from "../../components/sales/PaymentFilters";
+import PaymentsTable from "../../components/sales/PaymentsTable";
 
+export default function Payments() {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-2xl font-semibold">Payments</h1>
+
+      <PaymentSummaryCards />
+
+      <PaymentFilters />
+
+      <PaymentsTable />
+    </div>
+  );
+}
