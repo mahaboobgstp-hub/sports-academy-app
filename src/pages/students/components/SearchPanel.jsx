fa76b384f676ad94b4a62a55f9d17544b0e53e77
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 12:44:53 +0530

    Update SearchPanel.jsx

--- src/pages/students/components/SearchPanel.jsx
+++ src/pages/students/components/SearchPanel.jsx
@@ -1,1 +1,12 @@
+export default function SearchPanel() {
+  return (
+    <div style={{ marginBottom: 16 }}>
+      <input
+        placeholder="Search by Parent Mobile"
+        style={{ padding: 8, width: 250 }}
+      />
+      <button style={{ marginLeft: 8 }}>Search</button>
+    </div>
+  );
+}
 
