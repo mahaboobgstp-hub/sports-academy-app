b5fb84a07314291af53e5881c2393762d974e348
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 17:47:13 +0530

    Update StudentProfile.jsx

--- src/pages/students/profile/StudentProfile.jsx
+++ src/pages/students/profile/StudentProfile.jsx
@@ -3,12 +3,17 @@ import StudentOverview from "./StudentOverview";
 import SkillsPanel from "./SkillsPanel";
 import AttendancePanel from "./AttendancePanel";
 import CoachNotes from "./CoachNotes";
+import CurriculumPanel from "./CurriculumPanel";
+import DrillsPanel from "./DrillsPanel";
+
 
 export default function StudentProfile() {
   return (
     <div className="student-profile-page">
       <StudentOverview />
+      <CurriculumPanel />
       <SkillsPanel />
+      <DrillsPanel />
       <AttendancePanel />
       <CoachNotes />
     </div>
