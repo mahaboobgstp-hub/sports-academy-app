3215a885ff721ea0691e0a427f720a7679f7de9a
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 12:45:07 +0530

    Update StudentList.jsx

--- src/pages/students/components/StudentList.jsx
+++ src/pages/students/components/StudentList.jsx
@@ -1,1 +1,17 @@
+import StudentCard from "./StudentCard";
+
+export default function StudentList() {
+  const students = [
+    { id: 1, name: "Aarav", sport: "Football" },
+    { id: 2, name: "Diya", sport: "Badminton" }
+  ];
+
+  return (
+    <div>
+      {students.map(s => (
+        <StudentCard key={s.id} student={s} />
+      ))}
+    </div>
+  );
+}
 
