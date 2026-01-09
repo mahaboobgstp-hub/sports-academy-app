f1365607e0800763b0dc55286f46ba8734a8224d
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 12:45:54 +0530

    Update StudentProfile.jsx

--- src/pages/students/profile/StudentProfile.jsx
+++ src/pages/students/profile/StudentProfile.jsx
@@ -1,1 +1,19 @@
+import StudentOverview from "./StudentOverview";
+import SkillsPanel from "./SkillsPanel";
+import AttendancePanel from "./AttendancePanel";
+import CoachNotes from "./CoachNotes";
+import MessagePanel from "./MessagePanel";
+
+export default function StudentProfile() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h2>Student Profile</h2>
+      <StudentOverview />
+      <SkillsPanel />
+      <AttendancePanel />
+      <CoachNotes />
+      <MessagePanel />
+    </div>
+  );
+}
 
