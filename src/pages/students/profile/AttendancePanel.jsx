3f338901950f0da5ff1c1fff80cbb253a79f2f03
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 18:33:16 +0530

    Update AttendancePanel.jsx

--- src/pages/students/profile/AttendancePanel.jsx
+++ src/pages/students/profile/AttendancePanel.jsx
@@ -1,7 +1,26 @@
 const attendanceData = [
-  "P", "P", "A", "P", "P", "H", "P", "P", "P", "A",
-  "P", "P", "P", "H", "P", "A", "P", "P", "P", "P"
+  { day: "01", status: "P" },
+  { day: "02", status: "P" },
+  { day: "03", status: "A" },
+  { day: "04", status: "P" },
+  { day: "05", status: "P" },
+  { day: "06", status: "H" },
+  { day: "07", status: "P" },
+  { day: "08", status: "P" },
+  { day: "09", status: "P" },
+  { day: "10", status: "A" },
+  { day: "11", status: "P" },
+  { day: "12", status: "P" },
+  { day: "13", status: "P" },
+  { day: "14", status: "H" },
+  { day: "15", status: "P" },
+  { day: "16", status: "A" },
+  { day: "17", status: "P" },
+  { day: "18", status: "P" },
+  { day: "19", status: "P" },
+  { day: "20", status: "P" }
 ];
+
 // P = Present, A = Absent, H = Holiday
 
 export default function AttendancePanel() {
@@ -14,21 +33,24 @@ export default function AttendancePanel() {
         <div>Attendance: 83%</div>
       </div>
 
-      <div className="attendance-grid">
-        {attendanceData.map((status, index) => (
-          <div
-            key={index}
-            className={`attendance-cell ${status}`}
-            title={
-              status === "P"
-                ? "Present"
-                : status === "A"
-                ? "Absent"
-                : "Holiday"
-            }
-          />
-        ))}
-      </div>
+     <div className="attendance-grid">
+  {attendanceData.map((item, index) => (
+    <div
+      key={index}
+      className={`attendance-cell ${item.status}`}
+      title={
+        item.status === "P"
+          ? "Present"
+          : item.status === "A"
+          ? "Absent"
+          : "Holiday"
+      }
+    >
+      <span className="attendance-date">{item.day}</span>
+    </div>
+  ))}
+</div>
+
 
       <div className="attendance-legend">
         <span><i className="present" /> Present</span>
