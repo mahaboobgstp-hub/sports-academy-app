1f4bb9bb926f425627646535aa0696e5faf10809
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-09 19:58:22 +0530

    Update StudentProfile.jsx

--- src/pages/students/profile/StudentProfile.jsx
+++ src/pages/students/profile/StudentProfile.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import "./StudentProfile.css";
 import StudentOverview from "./StudentOverview";
 import SkillsPanel from "./SkillsPanel";
@@ -9,16 +10,19 @@ import QuestionsPanel from "./QuestionsPanel";
 
 
 
+
 export default function StudentProfile() {
+  const location = useLocation();
+const isCoach = new URLSearchParams(location.search).get("mode") === "coach";
   return (
     <div className="student-profile-page">
-      <StudentOverview />
-      <SkillsPanel />
+      <StudentOverview isCoach={isCoach} />
+      <SkillsPanel isCoach={isCoach} />
       <CurriculumPanel />
-      <DrillsPanel />
-      <AttendancePanel />
-      <CoachNotes />
-      <QuestionsPanel />
+      <DrillsPanel isCoach={isCoach} />
+      <AttendancePanel isCoach={isCoach} />
+      <CoachNotes isCoach={isCoach} />
+      <QuestionsPanel isCoach={isCoach} />
     </div>
   );
 }
