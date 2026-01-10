be2898d6b161c6f2c12047767de1dbd206828808
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-11 04:56:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,23 +93,9 @@ export default function App() {
 
              {/* CALENDAR */}
           
-          <div style={{ display: "flex", height: "100vh" }}>
-  <Sidebar />
-  <main style={{ flex: 1, padding: "16px", overflow: "auto" }}>
-    <Routes>
-      <Route path="/calendar" element={<CalendarPage />} />
-      {/* other routes */}
-    </Routes>
-  </main>
-</div>
-
-  
-  
-
-            
-            
-
-        </Route>
+          <Route path="/calendar" element={<CalendarPage />} />
+                        
+       </Route>
 
       </Routes>
     </BrowserRouter>
