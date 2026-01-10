96b647bca7efa5e402277749ff9aab9e68523be0
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-10 17:12:02 +0530

    Create SaveSettingsBar.jsx

---
+++ src/modules/settings/components/SaveSettingsBar.jsx
@@ -0,0 +1,9 @@
+export default function SaveSettingsBar() {
+  return (
+    <div className="pt-4 border-t flex justify-end">
+      <button className="bg-black text-white px-4 py-2 rounded">
+        Save Changes
+      </button>
+    </div>
+  );
+}
