a5a51b90c0a00d7652aabecca81f79904ecdeea3
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-10 16:42:25 +0530

    Update PermissionMatrix.jsx

--- src/modules/administration/components/roles/PermissionMatrix.jsx
+++ src/modules/administration/components/roles/PermissionMatrix.jsx
@@ -1,1 +1,27 @@
+export default function PermissionMatrix({ permissions }) {
+  return (
+    <table className="w-full text-sm border">
+      <thead>
+        <tr>
+          <th>Module</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(permissions).map(([module, actions]) => (
+          <tr key={module}>
+            <td>{module}</td>
+            <td>
+              {actions.map(a => (
+                <label key={a} className="mr-3">
+                  <input type="checkbox" /> {a}
+                </label>
+              ))}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
