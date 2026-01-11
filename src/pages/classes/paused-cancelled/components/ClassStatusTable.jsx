5f17966ece4682cfff1fd52ccc584cf8e30b482e
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-11 13:47:00 +0530

    Update ClassStatusTable.jsx

--- src/pages/classes/paused-cancelled/components/ClassStatusTable.jsx
+++ src/pages/classes/paused-cancelled/components/ClassStatusTable.jsx
@@ -35,7 +35,9 @@ export default function ClassStatusTable({ data }) {
               } hover:bg-blue-50 transition`}
             >
               <td className="p-4 font-medium">{item.className}</td>
-              <td className="p-4">{item.program}</td>
+              /*<td className="p-4">{item.program}</td>*/
+              <td className="p-4 bg-red-500 text-white"> {item.program} </td>
+
               <td className="p-4">{item.location}</td>
               <td className="p-4">{item.coach}</td>
               <td className="p-4">
