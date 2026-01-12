2ec22a710dfbc2c982f3a5c25f2ff1bec5b7d01e
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-12 22:41:52 +0530

    Update AppLayout.jsx

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
-export default function AppLayout() {
+export default function AppLayout({ children }) {
   return (
-         
- <div className="app-shell">
+ <div className="app-layout">
       <Sidebar />
-      <main className="main-content">
-        <Outlet />
-      </main>
+      <div className="content">
+         {children}
+    </div>    
     </div>
      );
 }
