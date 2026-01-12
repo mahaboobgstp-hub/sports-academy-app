18c94f9987c8ec8c708414508e914952d2dd3fa2
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-13 01:16:35 +0530

    Update salesRoutes.jsx

--- src/routes/salesRoutes.jsx
+++ src/routes/salesRoutes.jsx
@@ -7,6 +7,7 @@ import SalesLayout from "../layout/SalesLayout";
 import Payments from "../pages/sales/Payments";
 import ProductSales from "../pages/sales/ProductSales";
 import Invoices from "../pages/sales/Invoices";
+import Invoices from "../modules/invoices/pages/CreateInvoice";
 
 export default function SalesRoutes() {
   return (
@@ -18,7 +19,7 @@ export default function SalesRoutes() {
         {/* Sales sub-modules */}
         <Route path="payments" element={<Payments />} />
         <Route path="product-sales" element={<ProductSales />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="CreateInvoice" element={<Invoices />} />
       </Route>
     </Routes>
   );
