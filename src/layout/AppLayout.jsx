6f688cc6444d54113edd5bf764f8422c9a1922da
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-12 22:20:15 +0530

    Update AppLayout.jsx

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,18 @@
-  
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
 
 export default function AppLayout() {
   return (
-    <div className="app-shell">
-      <Sidebar />
-      <main className="main-content">
+    <div className="flex h-screen w-screen overflow-hidden">
+      {/* Sidebar */}
+      <aside className="w-64 bg-slate-900 text-white shrink-0">
+        Sports Academy ERP
+      </aside>
+
+      {/* MAIN CONTENT AREA */}
+      <div className="flex-1 overflow-y-auto bg-gray-100 p-6">
         <Outlet />
-      </main>
+      </div>
     </div>
   );
 }
+  
