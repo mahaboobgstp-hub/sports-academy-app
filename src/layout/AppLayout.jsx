358b1d22b20b79f29236a3fe96c2aecdff215671
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-12 21:47:28 +0530

    Update AppLayout.jsx

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,11 +4,16 @@ import "./AppLayout.css";
 
 export default function AppLayout() {
   return (
-    <div className="app-shell">
-      <Sidebar />
-      <div className="app-content">
+     <div className="flex h-screen">
+      {/* Sidebar */}
+      <aside className="w-64 bg-gray-900 text-white">
+        SIDEBAR
+      </aside>
+
+      {/* Main content */}
+      <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 }
