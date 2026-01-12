582a7be99dd0dce0e3ac37a9dabd288ebf13ce5b
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-12 22:09:06 +0530

    Update AppLayout.jsx

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,18 +1,12 @@
+  
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
-import "./AppLayout.css";
-
 export default function AppLayout() {
   return (
-     <div className="flex h-screen">
-      {/* Sidebar */}
-      <aside className="w-64 bg-gray-900 text-white">
-        SIDEBAR
-      </aside>
-
-      {/* Main content */}
-      <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
+    <div className="app-shell">
+      <Sidebar />
+      <main className="main-content">
         <Outlet />
       </main>
     </div>
