85a22f6aab288213ed4b5a8f5cad9f18dca42bc6
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-13 13:44:35 +0530

    Update PricingRulesTable.jsx

--- src/pricing-rules/components/PricingRulesTable.jsx
+++ src/pricing-rules/components/PricingRulesTable.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Card, CardContent } from "../../components/ui/card";
+//import { Card, CardContent } from "../../components/ui/card";
 
 import PricingRuleRow from "./PricingRuleRow";
 
