6238b3921c308d81d22b6b48c260f4dca1def8be
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-13 14:09:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
-* @type {import('tailwindcss').Config} 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+  },
+  corePlugins: {
+    preflight: false, // 🚑 THIS SAVES YOUR EXISTING CSS
   },
   plugins: [],
 };
