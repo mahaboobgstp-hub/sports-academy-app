0306959d60a11e05e207888919941aa456c37add
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-14 15:23:41 +0530

    Update ProgramPlanner.jsx

--- src/pages/planner/ProgramPlanner.jsx
+++ src/pages/planner/ProgramPlanner.jsx
@@ -464,7 +464,7 @@ const handleSaveProgram = (pIndex) => {
   Save Program
 </button>
             </div>
-            </div>
+           
 
             <button className="sub-btn" onClick={() => addLocation(pIndex)}>
               + Add Location
