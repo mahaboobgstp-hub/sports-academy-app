3c705dbb8398841b9a7fa1f8a1e095d411b210fe
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-15 20:21:43 +0530

    Update mockAttendance.js

--- src/pages/students/mock/mockAttendance.js
+++ src/pages/students/mock/mockAttendance.js
@@ -1,1 +1,20 @@
+const generateMockAttendance = () => {
+  const data = [];
+
+  for (let i = 1; i <= 90; i++) {
+    let status = "none";
+
+    if (i <= 20) {
+      if (i <= 16) status = "present";       // 80%
+      else if (i === 17) status = "absent";
+      else if (i === 18) status = "holiday";
+      else if (i === 19) status = "present";
+      else status = "cancelled";
+    }
+
+    data.push({ day: i, status });
+  }
+
+  return data;
+};
 
