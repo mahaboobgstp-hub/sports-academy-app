cf09f66c5f587fb3eecfc60195ea6221a02d8b71
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-16 01:14:31 +0530

    Update StudentOverview.jsx

--- src/pages/students/profile/StudentOverview.jsx
+++ src/pages/students/profile/StudentOverview.jsx
@@ -1,5 +1,6 @@
 //import studentImg from "../../../../assets/student.png";
 //import coachImg from "../../../../assets/coach.png";
+import EnrollmentInfo from "../components/EnrollmentInfo";
 
 export default function StudentOverview() {
   return (
@@ -18,6 +19,20 @@ export default function StudentOverview() {
             <div>Age: 11 · U-12 · Basketball</div>
           </div>
         </div>
+
+       
+ 
+  <EnrollmentInfo
+    status="active"
+    year="2025"
+    season="Summer"
+    location="Indiranagar"
+    program="U-12 Basketball"
+    classTime="Mon–Wed, 5:30–6:30 PM"
+  />
+  
+
+
 
         {/* Coach */}
         <div className="person">
