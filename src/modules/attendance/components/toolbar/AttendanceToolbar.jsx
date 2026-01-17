5416a93a0243a70cf9f83eae9f912f47eb234bda
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:27:15 +0530

    Update AttendanceToolbar.jsx

--- src/modules/attendance/components/toolbar/AttendanceToolbar.jsx
+++ src/modules/attendance/components/toolbar/AttendanceToolbar.jsx
@@ -1,1 +1,19 @@
+import MonthSelector from "./MonthSelector";
+import ExportButton from "./ExportButton";
+import BulkMarkToggle from "./BulkMarkToggle";
 
+export default function AttendanceToolbar(props) {
+  return (
+    <div className="attendance-toolbar">
+      <MonthSelector
+        month={props.month}
+        setMonth={props.setMonth}
+      />
+      <BulkMarkToggle
+        bulkMode={props.bulkMode}
+        setBulkMode={props.setBulkMode}
+      />
+      <ExportButton data={props.attendanceData} />
+    </div>
+  );
+}
