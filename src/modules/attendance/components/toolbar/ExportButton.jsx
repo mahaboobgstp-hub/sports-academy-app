20c2bae81c27937246b62bc5ce4b36faa0c8f6ae
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:28:22 +0530

    Update ExportButton.jsx

--- src/modules/attendance/components/toolbar/ExportButton.jsx
+++ src/modules/attendance/components/toolbar/ExportButton.jsx
@@ -1,1 +1,14 @@
+export default function ExportButton({ data }) {
+  const exportCSV = () => {
+    const csv = JSON.stringify(data);
+    const blob = new Blob([csv], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
 
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "attendance.txt";
+    a.click();
+  };
+
+  return <button onClick={exportCSV}>Export</button>;
+}
