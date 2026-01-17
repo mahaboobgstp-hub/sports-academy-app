5743e520c515e8e18979947405dcf18efa67d7e2
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:27:51 +0530

    Update MonthSelector.jsx

--- src/modules/attendance/components/toolbar/MonthSelector.jsx
+++ src/modules/attendance/components/toolbar/MonthSelector.jsx
@@ -1,1 +1,9 @@
-
+export default function MonthSelector({ month, setMonth }) {
+  return (
+    <input
+      type="month"
+      value={month}
+      onChange={(e) => setMonth(e.target.value)}
+    />
+  );
+}
