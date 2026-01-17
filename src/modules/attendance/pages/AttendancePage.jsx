f2bc34645f043e29a322cc5473d54964af2a191a
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:26:25 +0530

    Update AttendancePage.jsx

--- src/modules/attendance/pages/AttendancePage.jsx
+++ src/modules/attendance/pages/AttendancePage.jsx
@@ -1,1 +1,16 @@
+import AttendanceToolbar from "../components/toolbar/AttendanceToolbar";
+import AttendanceGrid from "../components/grid/AttendanceGrid";
+import AttendanceSummary from "../components/summary/AttendanceSummary";
+import useAttendance from "../hooks/useAttendance";
 
+export default function AttendancePage() {
+  const attendance = useAttendance();
+
+  return (
+    <div className="attendance-page">
+      <AttendanceToolbar {...attendance} />
+      <AttendanceGrid {...attendance} />
+      <AttendanceSummary {...attendance} />
+    </div>
+  );
+}
