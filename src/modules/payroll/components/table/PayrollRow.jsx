bdbc21abb5632b108036200ce170bc7c60a32b54
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:44:15 +0530

    Update PayrollRow.jsx

--- src/modules/payroll/components/table/PayrollRow.jsx
+++ src/modules/payroll/components/table/PayrollRow.jsx
@@ -1,1 +1,10 @@
-
+export default function PayrollRow({ emp, openDrawer }) {
+  return (
+    <tr onClick={() => openDrawer(emp)}>
+      <td>{emp.name}</td>
+      <td>{emp.gross}</td>
+      <td>{emp.deductions}</td>
+      <td>{emp.net}</td>
+    </tr>
+  );
+}
