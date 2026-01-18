5dd480fe373c89cdb25d1daecd0d00f0871db3dc
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:31:35 +0530

    Update EmployeeRow.jsx

--- src/modules/attendance/components/grid/EmployeeRow.jsx
+++ src/modules/attendance/components/grid/EmployeeRow.jsx
@@ -1,1 +1,16 @@
+import DayCell from "./DayCell";
 
+export default function EmployeeRow({ employee, days, markAttendance }) {
+  return (
+    <tr>
+      <td>{employee.name}</td>
+      {days.map((day) => (
+        <DayCell
+          key={day}
+          status={employee.attendance[day]}
+          onClick={() => markAttendance(employee.id, day)}
+        />
+      ))}
+    </tr>
+  );
+}
