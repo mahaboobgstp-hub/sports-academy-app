49ab855862a01e3831b4653f62a8a485324a67be
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:31:02 +0530

    Update AttendanceGrid.jsx

--- src/modules/attendance/components/grid/AttendanceGrid.jsx
+++ src/modules/attendance/components/grid/AttendanceGrid.jsx
@@ -1,1 +1,26 @@
+import EmployeeRow from "./EmployeeRow";
 
+export default function AttendanceGrid({ employees, days, markAttendance }) {
+  return (
+    <table className="attendance-grid">
+      <thead>
+        <tr>
+          <th>Employee</th>
+          {days.map((d) => (
+            <th key={d}>{d}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map((emp) => (
+          <EmployeeRow
+            key={emp.id}
+            employee={emp}
+            days={days}
+            markAttendance={markAttendance}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
