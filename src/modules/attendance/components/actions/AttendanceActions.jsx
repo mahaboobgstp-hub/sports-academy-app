d1cea3b132754d467ec507d0d63cfbab1bce2c7d
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:33:37 +0530

    Update AttendanceActions.jsx

--- src/modules/attendance/components/actions/AttendanceActions.jsx
+++ src/modules/attendance/components/actions/AttendanceActions.jsx
@@ -1,1 +1,5 @@
-
+export function toggleStatus(current) {
+  const order = ["present", "absent", "holiday", null];
+  const index = order.indexOf(current);
+  return order[(index + 1) % order.length];
+}
