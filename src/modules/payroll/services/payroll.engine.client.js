b6ae86e2062b7dec133d6505ec35ef130e30e34f
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:47:37 +0530

    Update payroll.engine.client.js

--- src/modules/payroll/services/payroll.engine.client.js
+++ src/modules/payroll/services/payroll.engine.client.js
@@ -1,1 +1,12 @@
+import { PF_PERCENT, TAX_PERCENT } from "../config/payroll.rules";
 
+export function calculateSalary(emp) {
+  const gross = emp.basic + emp.hra;
+  const deductions = (gross * (PF_PERCENT + TAX_PERCENT)) / 100;
+  return {
+    ...emp,
+    gross,
+    deductions,
+    net: gross - deductions
+  };
+}
