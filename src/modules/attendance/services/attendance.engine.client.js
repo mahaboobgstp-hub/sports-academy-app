703dd6aa77254e8206543dc3236dbb24a58615e4
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:35:27 +0530

    Update attendance.engine.client.js

--- src/modules/attendance/services/attendance.engine.client.js
+++ src/modules/attendance/services/attendance.engine.client.js
@@ -1,1 +1,15 @@
+export function calculateSummary(employees) {
+  let present = 0,
+    absent = 0,
+    holiday = 0;
 
+  employees.forEach((e) => {
+    Object.values(e.attendance).forEach((s) => {
+      if (s === "present") present++;
+      if (s === "absent") absent++;
+      if (s === "holiday") holiday++;
+    });
+  });
+
+  return { present, absent, holiday };
+}
