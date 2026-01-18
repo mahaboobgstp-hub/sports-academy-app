1a0c167440ad97ca44144014398900173f528188
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:42:12 +0530

    Update PayrollPage.jsx

--- src/modules/payroll/pages/PayrollPage.jsx
+++ src/modules/payroll/pages/PayrollPage.jsx
@@ -1,1 +1,18 @@
+import PayrollPeriodSelector from "../components/period/PayrollPeriodSelector";
+import PayrollEmployeeTable from "../components/table/PayrollEmployeeTable";
+import PayrollSummary from "../components/summary/PayrollSummary";
+import PayrollActions from "../components/actions/PayrollActions";
+import { usePayroll } from "../hooks/usePayroll";
 
+export default function PayrollPage() {
+  const payroll = usePayroll();
+
+  return (
+    <div style={{ padding: 24 }}>
+      <PayrollPeriodSelector {...payroll} />
+      <PayrollSummary {...payroll} />
+      <PayrollEmployeeTable {...payroll} />
+      <PayrollActions {...payroll} />
+    </div>
+  );
+}
