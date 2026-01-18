89d88f3d35ed7a002a48ee6c29b164dd5bec5768
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:42:45 +0530

    Update PayrollPeriodSelector.jsx

--- src/modules/payroll/components/period/PayrollPeriodSelector.jsx
+++ src/modules/payroll/components/period/PayrollPeriodSelector.jsx
@@ -1,1 +1,12 @@
-
+export default function PayrollPeriodSelector({ period, setPeriod }) {
+  return (
+    <div style={{ marginBottom: 16 }}>
+      <label>Payroll Period:</label>
+      <input
+        type="month"
+        value={period}
+        onChange={(e) => setPeriod(e.target.value)}
+      />
+    </div>
+  );
+}
