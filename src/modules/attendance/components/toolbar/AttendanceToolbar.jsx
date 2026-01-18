b964d1ade563f85a77209bd7463cea242dbff421
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 22:12:09 +0530

    Update AttendanceToolbar.jsx

--- src/modules/attendance/components/toolbar/AttendanceToolbar.jsx
+++ src/modules/attendance/components/toolbar/AttendanceToolbar.jsx
@@ -1,19 +1,34 @@
-import MonthSelector from "./MonthSelector";
-import ExportButton from "./ExportButton";
+import PeriodSelector from "./PeriodSelector";
+import OrgUnitFilter from "./OrgUnitFilter";
+import DepartmentFilter from "./DepartmentFilter";
+import GroupFilter from "./GroupFilter";
+import RoleFilter from "./RoleFilter";
 import BulkMarkToggle from "./BulkMarkToggle";
+import ExportButton from "./ExportButton";
 
-export default function AttendanceToolbar(props) {
+export default function AttendanceToolbar({
+  period,
+  setPeriod,
+  filters,
+  setFilters,
+  bulkMode,
+  setBulkMode,
+  attendanceData
+}) {
   return (
     <div className="attendance-toolbar">
-      <MonthSelector
-        month={props.month}
-        setMonth={props.setMonth}
-      />
-      <BulkMarkToggle
-        bulkMode={props.bulkMode}
-        setBulkMode={props.setBulkMode}
-      />
-      <ExportButton data={props.attendanceData} />
+      <div className="toolbar-left">
+        <PeriodSelector period={period} setPeriod={setPeriod} />
+        <OrgUnitFilter filters={filters} setFilters={setFilters} />
+        <DepartmentFilter filters={filters} setFilters={setFilters} />
+        <GroupFilter filters={filters} setFilters={setFilters} />
+        <RoleFilter filters={filters} setFilters={setFilters} />
+      </div>
+
+      <div className="toolbar-right">
+        <BulkMarkToggle bulkMode={bulkMode} setBulkMode={setBulkMode} />
+        <ExportButton data={attendanceData} />
+      </div>
     </div>
   );
 }
