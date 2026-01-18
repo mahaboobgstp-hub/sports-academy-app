5c0c845188bf7dc72563dcba886bd93e8dad1592
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:45:49 +0530

    Update PayrollActions.jsx

--- src/modules/payroll/components/actions/PayrollActions.jsx
+++ src/modules/payroll/components/actions/PayrollActions.jsx
@@ -1,1 +1,7 @@
-
+export default function PayrollActions({ processPayroll }) {
+  return (
+    <div style={{ marginTop: 16 }}>
+      <button onClick={processPayroll}>Process Payroll</button>
+    </div>
+  );
+}
