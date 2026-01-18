ee4dea40e165a59f000385596472b9534d5fd199
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-18 05:43:47 +0530

    Update PayrollEmployeeTable.jsx

--- src/modules/payroll/components/table/PayrollEmployeeTable.jsx
+++ src/modules/payroll/components/table/PayrollEmployeeTable.jsx
@@ -1,1 +1,21 @@
+import PayrollRow from "./PayrollRow";
 
+export default function PayrollEmployeeTable({ employees, openDrawer }) {
+  return (
+    <table width="100%" border="1" cellPadding="8">
+      <thead>
+        <tr>
+          <th>Employee</th>
+          <th>Gross</th>
+          <th>Deductions</th>
+          <th>Net Pay</th>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map(emp => (
+          <PayrollRow key={emp.id} emp={emp} openDrawer={openDrawer} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
